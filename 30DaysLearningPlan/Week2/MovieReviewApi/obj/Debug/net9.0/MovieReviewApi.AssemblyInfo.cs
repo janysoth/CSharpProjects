d37c5dcb0355f5d05cff1dc136c62d12a23cdf3e
Jonny Vorn Soth <janysoth@gmail.com>
2025-11-05 00:18:34 -0600

    Update assembly information and regenerate binaries; reflect version changes and update sourcelink

--- 30DaysLearningPlan/Week2/MovieReviewApi/obj/Debug/net9.0/MovieReviewApi.AssemblyInfo.cs
+++ 30DaysLearningPlan/Week2/MovieReviewApi/obj/Debug/net9.0/MovieReviewApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieReviewApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b9b159129aa2fe89ebc3405adffba421a48282c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ab024eab59a5aa5dc29b702a01cd023f99df9b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieReviewApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieReviewApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
